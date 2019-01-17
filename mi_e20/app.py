37d52f66c90ed3d9baad6797d9048c03c3f20620
n-visheratin <n-visheratin@agiks.ru>
2019-01-17 10:28:32 +0300

    update robot client

--- mi_e20/app.py
+++ mi_e20/app.py
@@ -31,6 +31,48 @@ class ServerBase(SimpleHTTPRequestHandler):
 	protocol_version = "HTTP/1.1"
 
 
+
+class RoborClient(Thread):
+	ip = None
+	tocken = None
+	_showInfo = None
+
+	def __init__(self, showInfo):
+		Thread.__init__(self)
+		self._showInfo = showInfo
+
+
+	def isValidate(self):
+		return True if self.ip and self.tocken else False
+
+	def setIP(self, ip):
+		self.ip = ip
+
+	def setToken(self, tocken):
+		self.tocken = tocken
+
+	def send(self, com):
+		try:
+			r = miio.Vacuum(self.ip, self.tocken)
+
+			if com == 'info':
+				self._showInfo(str(r.raw_command('get_status', [])))
+
+			elif com == 'info_sound':
+				self._showInfo(str(r.raw_command('get_sound_progress', [])))
+
+		except Exception as e:
+			self._showInfo(str(e))
+
+
+	def stop(self):
+		if self._server:
+			self._server.shutdown()
+			self._server = None
+
+
+
+
 class Server(Thread):
 	port = 80
 	ip = None
@@ -92,6 +134,9 @@ class _Player(Thread):
 
 
 Player = _Player()
+
+
+
 
 
 
@@ -198,10 +243,13 @@ class Row (QWidget):
 
 
 
+
 class About(QMainWindow):
 	def __init__(self, parent=None):
 		super(About, self).__init__()
 		uic.loadUi(form_about, self)
+
+
 
 
 class WindowApp(QMainWindow):
@@ -214,8 +262,7 @@ class WindowApp(QMainWindow):
 	_server_worker = None
 	_server_file = None
 
-	_robot_ip = None
-	_robot_tocken = None
+	_robot_clien = None
 
 	_conf_Def = 'DEFAULT'
 
@@ -228,6 +275,7 @@ class WindowApp(QMainWindow):
 		self.m_about.triggered.connect(self._to_about)
 
 		self._rows = list()
+		self._robot_clien = RoborClient(self.showInfo)
 
 		self.list_info_model = QStringListModel();
 		self.list_info.setModel(self.list_info_model);
@@ -235,7 +283,6 @@ class WindowApp(QMainWindow):
 		self.e_server_ip.textChanged.connect(self._on_server_ip)
 		self.e_robot_ip.textChanged.connect(self._on_robot_ip)
 		self.e_robot_tocken.textChanged.connect(self._on_robot_tocken)
-
 
 		self.btn_make.clicked.connect(self._on_make)
 
@@ -267,15 +314,17 @@ class WindowApp(QMainWindow):
 		self._server_ip = self._get_conf_param('server_ip')
 		self.e_server_ip.setText(self._server_ip)
 
-		self._robot_ip = self._get_conf_param('robot_ip')
-		self.e_robot_ip.setText(self._robot_ip)
-
-		self._robot_tocken = self._get_conf_param('robot_tocken')
-		self.e_robot_tocken.setText(self._robot_tocken)
-
 		self._server_file = self._get_conf_param('server_file')
 		self.l_server_file.setText(os.path.basename(self._server_file))
-		
+
+		robot_ip = self._get_conf_param('robot_ip')
+		self.e_robot_ip.setText(robot_ip)
+
+		robot_tocken = self._get_conf_param('robot_tocken')
+		self.e_robot_tocken.setText(robot_tocken)
+
+
+
 		self._server_check_state()
 		self._robot_check_state()
 
@@ -294,12 +343,10 @@ class WindowApp(QMainWindow):
 		return self._cfg.set(self._conf_Def, p, v)
 
 	def _on_robot_info(self):
-		r = miio.Vacuum(self._robot_ip, self._robot_tocken)
-		self.showInfo(str(r.raw_command('get_status', [])))
+		self._robot_clien.send('info')
 
 	def _on_robot_info_sound(self):
-		r = miio.Vacuum(self._robot_ip, self._robot_tocken)
-		self.showInfo(str(r.raw_command('get_sound_progress', [])))
+		self._robot_clien.send('info_sound')
 
 	def _on_pkg(self, b, pkg):
 		self._pkg_base = pkg
@@ -366,20 +413,21 @@ class WindowApp(QMainWindow):
 
 
 	def _on_robot_ip(self, ip):
-		self._robot_ip = ip
+		self._robot_clien.setIP(ip);
 		self._set_conf_param('robot_ip', ip)
 		self._robot_check_state()
 
 
 	def _on_robot_tocken(self, tocken):
-		self._robot_tocken = tocken
+		self._robot_clien.setToken(tocken);
 		self._set_conf_param('robot_tocken', tocken)
 		self._robot_check_state()
 
 
 	def _robot_check_state(self):
-		self.btn_robot_info.setEnabled(True if self._robot_ip and self._robot_tocken else False)
-		self.btn_robot_info_sound.setEnabled(True if self._robot_ip and self._robot_tocken else False)
+		b = self._robot_clien.isValidate()
+		self.btn_robot_info.setEnabled(b)
+		self.btn_robot_info_sound.setEnabled(b)
 
 
 	def _addRow(self, n):
@@ -397,9 +445,9 @@ class WindowApp(QMainWindow):
 
 	def showInfo(self, msg):
 		l = self.list_info_model.stringList()
-		l.append(msg)
+		l.insert(0, msg)
 		self.list_info_model.setStringList(l)
-		self.list_info.scrollToBottom()
+		self.list_info.scrollToTop()
 
 	def end(self):
 		with open(config_name, 'w') as f:
